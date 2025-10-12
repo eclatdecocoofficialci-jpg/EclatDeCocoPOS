364263278bed1f11cfa7dcd1299dd2cd26c8bf04
eclatdecocoofficialci-jpg <eclatdecocoofficialci@gmail.com>
2025-10-12 03:59:52 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,26 @@
-const CACHE_NAME = 'coco-pos-cache-v1';
+const CACHE_NAME = 'coco-pos-cache-v2';
 const FILES_TO_CACHE = [
-  './EclatDeCocoPOS.html',
+  './index.html',
   './manifest.json',
-  './eclat_de_coco_official.png'
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
