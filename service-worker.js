5a9ee5551c7935ec4bd1c759873510c2dfb129ad
Eclatdecoco.official <eclatdecocoofficialci@gmail.com>
2026-04-29 21:02:13 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,42 @@
+const CACHE_NAME = "eclat-de-coco-pos-v1";
 
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/pos.html",
+  "/style.css",
+  "/script.js"
+];
+
+/* INSTALL */
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+/* ACTIVATE */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+/* FETCH (offline mode) */
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
